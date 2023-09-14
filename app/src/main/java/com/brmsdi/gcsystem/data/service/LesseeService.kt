096f81a77a9dcda7186617078dc1b746d674141b
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-14 00:39:50 -0400

    Api event not streaming

--- app/src/main/java/com/brmsdi/gcsystem/data/service/LesseeService.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/service/LesseeService.kt
@@ -3,15 +3,14 @@ package com.brmsdi.gcsystem.data.service
 import com.brmsdi.gcsystem.data.constants.Constant.ENDPOINT.CHANGE_PASSWORD_LESSEE
 import com.brmsdi.gcsystem.data.constants.Constant.ENDPOINT.REQUEST_CODE_LESSEE
 import com.brmsdi.gcsystem.data.constants.Constant.ENDPOINT.SEND_CODE_LESSEES
+import com.brmsdi.gcsystem.data.dto.ResponseDTO
 import com.brmsdi.gcsystem.data.dto.TokenChangePasswordDTO
 import com.brmsdi.gcsystem.data.dto.TokenDTO
-import okhttp3.ResponseBody
 import retrofit2.Call
 import retrofit2.http.Body
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Query
-import retrofit2.http.Streaming
 
 
 /**
@@ -21,15 +20,12 @@ import retrofit2.http.Streaming
  */
 
 interface LesseeService {
-
-    @Streaming
     @POST(REQUEST_CODE_LESSEE)
-    fun requestCode(@Query("email") email : String) : Call<ResponseBody>
+    fun requestCode(@Query("email") email : String) : Call<ResponseDTO>
 
     @POST(SEND_CODE_LESSEES)
     fun sendCode(@Query("email") email: String, @Query("code") code: String) : Call<TokenDTO>
 
-    @Streaming
     @PUT(CHANGE_PASSWORD_LESSEE)
-    fun changePassword(@Body tokenChangePasswordDTO: TokenChangePasswordDTO): Call<ResponseBody>
+    fun changePassword(@Body tokenChangePasswordDTO: TokenChangePasswordDTO): Call<ResponseDTO>
 }
