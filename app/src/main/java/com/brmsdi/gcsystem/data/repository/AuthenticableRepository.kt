07703aed23fefd48f327bdf4be6ea46de7ff0be0
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-14 00:40:20 -0400

    Api event not streaming

--- app/src/main/java/com/brmsdi/gcsystem/data/repository/AuthenticableRepository.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/repository/AuthenticableRepository.kt
@@ -2,8 +2,8 @@ package com.brmsdi.gcsystem.data.repository
 
 import com.brmsdi.gcsystem.data.dto.TokenChangePasswordDTO
 import com.brmsdi.gcsystem.data.listeners.APIEvent
-import com.brmsdi.gcsystem.data.listeners.APIEventStringAndJSON
 import com.brmsdi.gcsystem.data.dto.ChangePasswordDataDTO
+import com.brmsdi.gcsystem.data.dto.ResponseDTO
 import com.brmsdi.gcsystem.data.dto.TokenDTO
 
 /**
@@ -16,9 +16,9 @@ interface AuthenticableRepository : CallRepository {
 
     fun authenticate(cpf: String, password: String, apiEvent: APIEvent<TokenDTO>)
 
-    fun requestCode(email: String, event: APIEventStringAndJSON)
+    fun requestCode(email: String, event: APIEvent<ResponseDTO>)
 
     fun sendCode(changePasswordDataDTO: ChangePasswordDataDTO, apiEvent: APIEvent<TokenDTO>)
 
-    fun changePassword(tokenChangePasswordDTO: TokenChangePasswordDTO, event:APIEventStringAndJSON)
+    fun changePassword(tokenChangePasswordDTO: TokenChangePasswordDTO, event: APIEvent<ResponseDTO>)
 }
