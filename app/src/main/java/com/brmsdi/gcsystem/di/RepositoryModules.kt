978bcf592b11990a0b8d1756f4dad6249071c789
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-19 22:27:58 -0400

    EmployeeRepository and LesseeRepository added

--- app/src/main/java/com/brmsdi/gcsystem/di/RepositoryModules.kt
+++ app/src/main/java/com/brmsdi/gcsystem/di/RepositoryModules.kt
@@ -1,6 +1,8 @@
 package com.brmsdi.gcsystem.di
 
 import com.brmsdi.gcsystem.data.repository.AuthenticableRepository
+import com.brmsdi.gcsystem.data.repository.EmployeeRepository
+import com.brmsdi.gcsystem.data.repository.LesseeRepository
 import com.brmsdi.gcsystem.data.repository.impl.EmployeeRepositoryImpl
 import com.brmsdi.gcsystem.data.repository.impl.LesseeRepositoryImpl
 import com.brmsdi.gcsystem.ui.utils.AuthType.*
@@ -10,4 +12,6 @@ import org.koin.dsl.module
 val RepositoryModules = module {
     single<AuthenticableRepository>(named(EMPLOYEE.type)) { EmployeeRepositoryImpl() }
     single<AuthenticableRepository>(named(LESSEE.type)) { LesseeRepositoryImpl() }
+    single<EmployeeRepository>(named(EMPLOYEE.type)) { EmployeeRepositoryImpl() }
+    single<LesseeRepository>(named(LESSEE.type)) { LesseeRepositoryImpl() }
 }
