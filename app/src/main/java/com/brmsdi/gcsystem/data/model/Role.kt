7492da466b1948c78870c2ec2884b6d64d9980f7
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-19 22:28:18 -0400

    Created

---
+++ app/src/main/java/com/brmsdi/gcsystem/data/model/Role.kt
@@ -0,0 +1,13 @@
+package com.brmsdi.gcsystem.data.model
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
+data class Role(
+    @SerializedName("id")
+    var id: Int,
+    @SerializedName("name")
+    var name: String
+): Parcelable
