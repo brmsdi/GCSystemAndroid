d2b3532d350c74723817e8c75e1ec159d4ca3c00
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-19 22:30:17 -0400

    Params Employee objects

--- app/src/main/java/com/brmsdi/gcsystem/data/model/Employee.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/model/Employee.kt
@@ -3,9 +3,22 @@ package com.brmsdi.gcsystem.data.model
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
+import java.util.Date
 
 @Parcelize
 data class Employee(
+    @SerializedName("id")
+    var id: Int,
     @SerializedName("name")
-    var name: String
+    var name: String,
+    @SerializedName("cpf")
+    var cpf: String,
+    @SerializedName("birthDate")
+    var birthDate: Date,
+    @SerializedName("email")
+    var email: String,
+    @SerializedName("role")
+    var role: Role,
+    @SerializedName("hiringDate")
+    var hiringDate: Date
 ) : Parcelable
