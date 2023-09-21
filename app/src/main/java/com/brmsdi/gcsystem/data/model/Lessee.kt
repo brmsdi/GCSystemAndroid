a5f6d3c6b7740302a109b51cdbaec3fb98922c0b
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-20 21:24:41 -0400

    Added params

--- app/src/main/java/com/brmsdi/gcsystem/data/model/Lessee.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/model/Lessee.kt
@@ -3,11 +3,22 @@ package com.brmsdi.gcsystem.data.model
 import android.os.Parcelable
 import com.google.gson.annotations.SerializedName
 import kotlinx.parcelize.Parcelize
+import java.util.Date
 
 @Parcelize
 class Lessee(
+    @SerializedName("id")
+    var id: Int,
     @SerializedName("name")
     var name: String,
+    @SerializedName("cpf")
+    var cpf: String,
+    @SerializedName("birthDate")
+    var birthDate: Date,
+    @SerializedName("email")
+    var email: String,
+    @SerializedName("contactNumber")
+    var contactNumber: String,
     @SerializedName("debts")
     var debts: MutableList<Debt>
 ) : Parcelable
