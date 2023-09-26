ac88c858daac9b56e00e899e14c6d32df6990f5d
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-25 23:29:42 -0400

    Save Repair Request

--- app/src/main/java/com/brmsdi/gcsystem/data/service/RepairRequestService.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/service/RepairRequestService.kt
@@ -1,9 +1,14 @@
 package com.brmsdi.gcsystem.data.service
 
 import com.brmsdi.gcsystem.data.constants.Constant.ENDPOINT.DATA_SCREEN_NEW_REPAIR_REQUEST
+import com.brmsdi.gcsystem.data.constants.Constant.ENDPOINT.NEW_REPAIR_REQUEST
+import com.brmsdi.gcsystem.data.dto.RepairRequestRegisterDTO
 import com.brmsdi.gcsystem.data.dto.RepairRequestRegisterDataDTO
+import com.brmsdi.gcsystem.data.model.RepairRequest
 import retrofit2.Call
+import retrofit2.http.Body
 import retrofit2.http.GET
+import retrofit2.http.POST
 
 
 /**
@@ -16,4 +21,7 @@ interface RepairRequestService {
 
     @GET(DATA_SCREEN_NEW_REPAIR_REQUEST)
     fun loadDataNewRepairRequest() : Call<RepairRequestRegisterDataDTO>
+
+    @POST(NEW_REPAIR_REQUEST)
+    fun save(@Body repairRequestRegisterDTO: RepairRequestRegisterDTO) : Call<RepairRequest>
 }
