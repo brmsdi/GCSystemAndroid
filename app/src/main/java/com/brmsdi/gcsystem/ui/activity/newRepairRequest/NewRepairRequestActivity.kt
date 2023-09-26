14da48c9bb94c58c56ec662a2fe41ef9f08beff0
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-25 23:31:13 -0400

    Save Repair Request

--- app/src/main/java/com/brmsdi/gcsystem/ui/activity/newRepairRequest/NewRepairRequestActivity.kt
+++ app/src/main/java/com/brmsdi/gcsystem/ui/activity/newRepairRequest/NewRepairRequestActivity.kt
@@ -7,24 +7,22 @@ import android.view.View.OnClickListener
 import android.widget.ArrayAdapter
 import com.brmsdi.gcsystem.R
 import com.brmsdi.gcsystem.data.dto.CondominiumSpinnerDTO
+import com.brmsdi.gcsystem.data.dto.RepairRequestRegisterDTO
 import com.brmsdi.gcsystem.data.dto.RepairRequestRegisterDataDTO
 import com.brmsdi.gcsystem.data.dto.SpinnerDTO
 import com.brmsdi.gcsystem.data.dto.TypeProblemSpinnerDTO
 import com.brmsdi.gcsystem.data.model.Condominium
-import com.brmsdi.gcsystem.data.model.RepairRequest
 import com.brmsdi.gcsystem.data.model.TypeProblem
 import com.brmsdi.gcsystem.databinding.ActivityNewRepairRequestBinding
-import com.brmsdi.gcsystem.ui.utils.Mock.Companion.lesseeList
-import com.brmsdi.gcsystem.ui.utils.Mock.Companion.statusList
+import com.brmsdi.gcsystem.ui.utils.ProgressBarOnApp
 import com.brmsdi.gcsystem.ui.utils.TextUtils.Companion.displayMessage
 import com.brmsdi.gcsystem.ui.utils.TextUtils.Companion.fieldsIsNotEmpty
 import org.koin.androidx.viewmodel.ext.android.viewModel
-import java.util.Date
 
-class NewRepairRequestActivity : AppCompatActivity(), OnClickListener {
+class NewRepairRequestActivity : AppCompatActivity(), OnClickListener, ProgressBarOnApp {
     private val viewModel by viewModel<NewRepairRequestViewModel>()
     private lateinit var binding: ActivityNewRepairRequestBinding
-    private lateinit var repairRequest: RepairRequest
+    private lateinit var repairRequest: RepairRequestRegisterDTO
     private lateinit var spinnerAdapterCondominium: ArrayAdapter<SpinnerDTO<Condominium>>
     private lateinit var spinnerAdapterTypeProblem: ArrayAdapter<SpinnerDTO<TypeProblem>>
 
@@ -55,10 +53,8 @@ class NewRepairRequestActivity : AppCompatActivity(), OnClickListener {
             binding.buttonSave.id -> {
                 repairRequest = getFields()
                 if (!fieldsIsNotEmpty(
-                        repairRequest.condominium.name,
-                        repairRequest.apartmentNumber,
                         repairRequest.problemDescription,
-                        repairRequest.typeProblem.name
+                        repairRequest.apartmentNumber,
                     )
                 ) {
                     displayMessage(this, getString(R.string.fields_empty))
@@ -70,17 +66,32 @@ class NewRepairRequestActivity : AppCompatActivity(), OnClickListener {
     }
 
     private fun observe() {
+        viewModel.saved.observe(this) {
+            if (it.status()) {
+                displayMessage(this, getString(R.string.new_repair_request_success))
+                clearScreen()
+                showOrHideView(binding.progressSave, false)
+                showOrHideView(binding.buttonSave, true)
+            }
+        }
+
         viewModel.screenData.observe(this) {
             loadSpinner(it)
+            showOrHideView(binding.progressSave, false)
+            showOrHideView(binding.buttonSave, true)
         }
 
         viewModel.error.observe(this) {
             displayMessage(this, it.message())
+            showOrHideView(binding.progressSave, false)
+            showOrHideView(binding.buttonSave, true)
         }
     }
 
-    private fun save(repairRequest: RepairRequest) {
-        displayMessage(this, getString(R.string.new_repair_request_success))
+    private fun save(repairRequest: RepairRequestRegisterDTO) {
+        viewModel.save(repairRequest)
+        showOrHideView(binding.progressSave, true)
+        showOrHideView(binding.buttonSave, false)
     }
 
     private fun addAction() {
@@ -88,19 +99,14 @@ class NewRepairRequestActivity : AppCompatActivity(), OnClickListener {
         binding.buttonSave.setOnClickListener(this)
     }
 
-    private fun getFields(): RepairRequest {
+    private fun getFields(): RepairRequestRegisterDTO {
         val typeProblemSpinnerDTO = binding.spinnerTypeProblem.selectedItem as TypeProblemSpinnerDTO
         val condominiumSpinnerDTO = binding.spinnerCondominium.selectedItem as CondominiumSpinnerDTO
-        return RepairRequest(
+        return RepairRequestRegisterDTO(
             problemDescription = binding.editDescriptionProblem.text.toString(),
-            date = Date(),
-            typeProblem = typeProblemSpinnerDTO.getModel(),
-            lessee = lesseeList()[0],
-            condominium = condominiumSpinnerDTO.getModel(),
-            status = statusList()[0],
-            items = null,
+            condominiumID = condominiumSpinnerDTO.getModel().id,
             apartmentNumber = binding.editApartment.text.toString(),
-            orderService = null
+            typeProblemID = typeProblemSpinnerDTO.getModel().id
         )
     }
 
@@ -111,5 +117,10 @@ class NewRepairRequestActivity : AppCompatActivity(), OnClickListener {
     private fun loadSpinner(data: RepairRequestRegisterDataDTO) {
         spinnerAdapterCondominium.addAll(data.condominiums.map { CondominiumSpinnerDTO(it) })
         spinnerAdapterTypeProblem.addAll(data.typeProblems.map { TypeProblemSpinnerDTO(it) })
+    }
+
+    private fun clearScreen() {
+        binding.editApartment.setText("")
+        binding.editDescriptionProblem.setText("")
     }
 }
--- app/src/main/java/com/brmsdi/gcsystem/ui/activity/newRepairRequest/NewRepairRequestViewModel.kt
+++ app/src/main/java/com/brmsdi/gcsystem/ui/activity/newRepairRequest/NewRepairRequestViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.brmsdi.gcsystem.R
+import com.brmsdi.gcsystem.data.dto.RepairRequestRegisterDTO
 import com.brmsdi.gcsystem.data.dto.RepairRequestRegisterDataDTO
 import com.brmsdi.gcsystem.data.dto.ResponseRequestDTO
 import com.brmsdi.gcsystem.data.dto.ValidationModelDTO
 import com.brmsdi.gcsystem.data.listeners.APIEvent
 import com.brmsdi.gcsystem.data.model.RepairRequest
 import com.brmsdi.gcsystem.data.repository.RepairRequestRepository
+import com.brmsdi.gcsystem.ui.utils.TextUtils
 import retrofit2.Response
 import java.net.ConnectException
 
@@ -19,25 +21,57 @@ import java.net.ConnectException
  * @author Wisley Bruno Marques França
  * @since 1
  */
-class NewRepairRequestViewModel(application: Application, val repairRequestRepository: RepairRequestRepository) :
+class NewRepairRequestViewModel(
+    application: Application,
+    private val repairRequestRepository: RepairRequestRepository
+) :
     AndroidViewModel(application = application) {
     private val _screenData = MutableLiveData<RepairRequestRegisterDataDTO>()
     val screenData: LiveData<RepairRequestRegisterDataDTO> = _screenData
     private val _error = MutableLiveData<ValidationModelDTO>()
     val error: LiveData<ValidationModelDTO> = _error
-    fun save(repairRequest: RepairRequest) {
-        TODO("CONTINUAR")
+    private val _saved = MutableLiveData<ValidationModelDTO>()
+    val saved: LiveData<ValidationModelDTO> = _saved
+    fun save(repairRequest: RepairRequestRegisterDTO) {
+        repairRequestRepository.save(repairRequest, object : APIEvent<RepairRequest> {
+            override fun onResponse(model: RepairRequest) {
+                _saved.value = ValidationModelDTO()
+            }
+
+            override fun onError(response: Response<RepairRequest>) {
+                response.errorBody()?.string()?.let {
+                    val responseRequestDTO =
+                        TextUtils.jsonToObject(it, ResponseRequestDTO::class.java)
+                    _error.value = ValidationModelDTO(responseRequestDTO.errors[0].message)
+                }
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                val cause = throwable.cause
+                if (cause is ConnectException) {
+                    _error.value =
+                        ValidationModelDTO(getApplication<Application>().getString(R.string.ERROR_CONNECTION))
+                } else {
+                    _error.value =
+                        ValidationModelDTO(getApplication<Application>().getString(R.string.ERROR_UNEXPECTED))
+                }
+            }
+        })
     }
 
     fun loadDataNewRepairRequest() {
-        repairRequestRepository.loadDataNewRepairRequest(object : APIEvent<RepairRequestRegisterDataDTO> {
+        repairRequestRepository.loadDataNewRepairRequest(object :
+            APIEvent<RepairRequestRegisterDataDTO> {
             override fun onResponse(model: RepairRequestRegisterDataDTO) {
                 _screenData.value = model
             }
 
             override fun onError(response: Response<RepairRequestRegisterDataDTO>) {
-                _error.value =
-                    ValidationModelDTO(getApplication<Application>().getString(R.string.ERROR_UNEXPECTED))
+                response.errorBody()?.string()?.let {
+                    val responseRequestDTO =
+                        TextUtils.jsonToObject(it, ResponseRequestDTO::class.java)
+                    _error.value = ValidationModelDTO(responseRequestDTO.errors[0].message)
+                }
             }
 
             override fun onFailure(throwable: Throwable) {
