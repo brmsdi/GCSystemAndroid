c36cfe6745131c8756509045e3e12d7bea9fb893
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-25 23:30:22 -0400

    Fields added

--- app/src/main/java/com/brmsdi/gcsystem/data/dto/RepairRequestRegisterDTO.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/dto/RepairRequestRegisterDTO.kt
@@ -3,6 +3,12 @@ package com.brmsdi.gcsystem.data.dto
 import com.google.gson.annotations.SerializedName
 
 data class RepairRequestRegisterDTO(
-    @SerializedName("")
-    val id: Int
+    @SerializedName("problemDescription")
+    val problemDescription: String,
+    @SerializedName("condominiumID")
+    val condominiumID: Int,
+    @SerializedName("apartmentNumber")
+    val apartmentNumber: String,
+    @SerializedName("typeProblemID")
+    val typeProblemID: Int,
 )
