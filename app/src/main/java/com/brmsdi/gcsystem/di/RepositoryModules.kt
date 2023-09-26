7b438bcfc69b09ed713b552a2d4b80d0da1c9c86
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-25 21:32:12 -0400

    RepairRequest single

--- app/src/main/java/com/brmsdi/gcsystem/di/RepositoryModules.kt
+++ app/src/main/java/com/brmsdi/gcsystem/di/RepositoryModules.kt
@@ -3,8 +3,10 @@ package com.brmsdi.gcsystem.di
 import com.brmsdi.gcsystem.data.repository.AuthenticableRepository
 import com.brmsdi.gcsystem.data.repository.EmployeeRepository
 import com.brmsdi.gcsystem.data.repository.LesseeRepository
+import com.brmsdi.gcsystem.data.repository.RepairRequestRepository
 import com.brmsdi.gcsystem.data.repository.impl.EmployeeRepositoryImpl
 import com.brmsdi.gcsystem.data.repository.impl.LesseeRepositoryImpl
+import com.brmsdi.gcsystem.data.repository.impl.RepairRequestRepositoryImpl
 import com.brmsdi.gcsystem.ui.utils.AuthType.*
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
@@ -14,4 +16,5 @@ val RepositoryModules = module {
     single<AuthenticableRepository>(named(LESSEE.type)) { LesseeRepositoryImpl() }
     single<EmployeeRepository>(named(EMPLOYEE.type)) { EmployeeRepositoryImpl() }
     single<LesseeRepository>(named(LESSEE.type)) { LesseeRepositoryImpl() }
+    single<RepairRequestRepository> { RepairRequestRepositoryImpl() }
 }
