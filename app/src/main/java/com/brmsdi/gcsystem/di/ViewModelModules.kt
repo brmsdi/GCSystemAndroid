0dd9fc44a02d0229b47df688abb7da6281cc0853
Wisley Bruno <srwisley.dev@gmail.com>
2023-09-26 23:39:44 -0400

    View model repair request module

--- app/src/main/java/com/brmsdi/gcsystem/di/ViewModelModules.kt
+++ app/src/main/java/com/brmsdi/gcsystem/di/ViewModelModules.kt
@@ -1,9 +1,11 @@
 package com.brmsdi.gcsystem.di
 
 import com.brmsdi.gcsystem.ui.activity.newRepairRequest.NewRepairRequestViewModel
+import com.brmsdi.gcsystem.ui.fragment.repairRequest.RepairRequestViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val ViewModelModules = module {
     viewModel { NewRepairRequestViewModel(get(), get()) }
+    viewModel { RepairRequestViewModel(get(), get()) }
 }
