76dbe4f87c340ec6a54a2272ed14c3a6804eba20
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-03 00:24:18 -0400

    Equals and HashCode

--- app/src/main/java/com/brmsdi/gcsystem/data/model/Employee.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/model/Employee.kt
@@ -21,4 +21,23 @@ data class Employee(
     var role: Role,
     @SerializedName("hiringDate")
     var hiringDate: Date
-) : Parcelable
+) : Parcelable {
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+        other as Employee
+        return id == other.id && cpf == other.cpf
+    }
+
+    override fun hashCode(): Int {
+        var result = id
+        result = 31 * result + (name?.hashCode() ?: 0)
+        result = 31 * result + (cpf?.hashCode() ?: 0)
+        result = 31 * result + (birthDate?.hashCode() ?: 0)
+        result = 31 * result + (email?.hashCode() ?: 0)
+        result = 31 * result + (role?.hashCode() ?: 0)
+        result = 31 * result + (hiringDate?.hashCode() ?: 0)
+        return result
+    }
+}
