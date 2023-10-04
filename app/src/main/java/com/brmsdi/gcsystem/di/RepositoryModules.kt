15d3ed42f31d3500eeb57e6ea23a219077b6d385
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-03 21:29:49 -0400

    Debt repository dependency

--- app/src/main/java/com/brmsdi/gcsystem/di/RepositoryModules.kt
+++ app/src/main/java/com/brmsdi/gcsystem/di/RepositoryModules.kt
@@ -1,9 +1,11 @@
 package com.brmsdi.gcsystem.di
 
 import com.brmsdi.gcsystem.data.repository.AuthenticableRepository
+import com.brmsdi.gcsystem.data.repository.DebtRepository
 import com.brmsdi.gcsystem.data.repository.EmployeeRepository
 import com.brmsdi.gcsystem.data.repository.LesseeRepository
 import com.brmsdi.gcsystem.data.repository.RepairRequestRepository
+import com.brmsdi.gcsystem.data.repository.impl.DebtRepositoryImpl
 import com.brmsdi.gcsystem.data.repository.impl.EmployeeRepositoryImpl
 import com.brmsdi.gcsystem.data.repository.impl.LesseeRepositoryImpl
 import com.brmsdi.gcsystem.data.repository.impl.RepairRequestRepositoryImpl
@@ -17,4 +19,5 @@ val RepositoryModules = module {
     single<EmployeeRepository>(named(EMPLOYEE.type)) { EmployeeRepositoryImpl() }
     single<LesseeRepository>(named(LESSEE.type)) { LesseeRepositoryImpl() }
     single<RepairRequestRepository> { RepairRequestRepositoryImpl() }
+    single<DebtRepository> { DebtRepositoryImpl() }
 }
