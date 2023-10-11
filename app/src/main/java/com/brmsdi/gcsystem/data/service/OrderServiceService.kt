7a30517145a69d9ea1f320a1fb29cb178efd2830
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-11 17:52:06 -0400

    Search order service

--- app/src/main/java/com/brmsdi/gcsystem/data/service/OrderServiceService.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/service/OrderServiceService.kt
@@ -1,6 +1,7 @@
 package com.brmsdi.gcsystem.data.service
 
 import com.brmsdi.gcsystem.data.constants.Constant.ENDPOINT.ORDER_SERVICES_EMPLOYEE
+import com.brmsdi.gcsystem.data.constants.Constant.ENDPOINT.SEARCH_ORDER_SERVICE
 import com.brmsdi.gcsystem.data.dto.PaginationOrderServiceDTO
 import retrofit2.Call
 import retrofit2.http.GET
@@ -26,4 +27,17 @@ interface OrderServiceService {
      */
     @GET(ORDER_SERVICES_EMPLOYEE)
     fun loadOrderService(@QueryMap params: Map<String, String>) : Call<PaginationOrderServiceDTO>
+
+    /**
+     * Realiza uma busca na lista de ordem de serviço com paginação.
+     *
+     * @param params Um mapa de parâmetros de consulta opcionais.
+     *               Pode incluir os seguintes dados:
+     *               - "page": Número da página desejada (opcional).
+     *               - "size": Tamanho da página (número de itens por página) (opcional).
+     *               - "idOrderService": Parâmetro de pesquisa (Obrigatório)
+     * @return Um objeto [Call] que pode ser usado para fazer a solicitação HTTP.
+     */
+    @GET(SEARCH_ORDER_SERVICE)
+    fun search(@QueryMap params: Map<String, String> = mapOf()): Call<PaginationOrderServiceDTO>
 }
