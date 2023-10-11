75efd322596d6f851a6c13d2ade97b8001817dff
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-11 19:57:40 -0400

    Inject order service repo

--- app/src/main/java/com/brmsdi/gcsystem/ui/activity/detailOrderService/DetailOrderServiceViewModel.kt
+++ app/src/main/java/com/brmsdi/gcsystem/ui/activity/detailOrderService/DetailOrderServiceViewModel.kt
@@ -2,7 +2,7 @@ package com.brmsdi.gcsystem.ui.activity.detailOrderService
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import com.brmsdi.gcsystem.data.service.OrderServiceService
+import com.brmsdi.gcsystem.data.repository.OrderServiceRepository
 
 
 /**
@@ -10,5 +10,5 @@ import com.brmsdi.gcsystem.data.service.OrderServiceService
  * @author Wisley Bruno Marques França
  * @since 1
  */
-class DetailOrderServiceViewModel(application: Application, orderServiceService: OrderServiceService) : AndroidViewModel(application) {
+class DetailOrderServiceViewModel(application: Application, private val orderServiceRepository: OrderServiceRepository) : AndroidViewModel(application) {
 }
