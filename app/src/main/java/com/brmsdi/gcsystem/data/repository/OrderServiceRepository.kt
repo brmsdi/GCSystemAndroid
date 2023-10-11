de0fa438a3d4efabd9a0bb1ee1e2ebec891df90a
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-11 17:12:59 -0400

    Created

---
+++ app/src/main/java/com/brmsdi/gcsystem/data/repository/OrderServiceRepository.kt
@@ -0,0 +1,15 @@
+package com.brmsdi.gcsystem.data.repository
+
+import com.brmsdi.gcsystem.data.dto.PaginationOrderServiceDTO
+import com.brmsdi.gcsystem.data.listeners.APIEvent
+
+
+/**
+ *
+ * @author Wisley Bruno Marques França
+ * @since 1
+ */
+
+interface OrderServiceRepository : CallRepository {
+    fun loadOrderServices(params: Map<String, String>, event: APIEvent<PaginationOrderServiceDTO>)
+}
