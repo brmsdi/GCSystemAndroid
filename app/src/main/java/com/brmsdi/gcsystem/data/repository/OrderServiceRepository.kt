f7ed3a4c19348697827dacf3577b2c3b024bb6cb
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-11 19:51:20 -0400

    Details order service method

--- app/src/main/java/com/brmsdi/gcsystem/data/repository/OrderServiceRepository.kt
+++ app/src/main/java/com/brmsdi/gcsystem/data/repository/OrderServiceRepository.kt
@@ -2,6 +2,7 @@ package com.brmsdi.gcsystem.data.repository
 
 import com.brmsdi.gcsystem.data.dto.PaginationOrderServiceDTO
 import com.brmsdi.gcsystem.data.listeners.APIEvent
+import com.brmsdi.gcsystem.data.model.OrderService
 
 
 /**
@@ -14,4 +15,6 @@ interface OrderServiceRepository : CallRepository {
     fun loadOrderServices(params: Map<String, String>, event: APIEvent<PaginationOrderServiceDTO>)
 
     fun search(params: Map<String, String>, event: APIEvent<PaginationOrderServiceDTO>)
+
+    fun details(id: Int, event: APIEvent<OrderService>)
 }
