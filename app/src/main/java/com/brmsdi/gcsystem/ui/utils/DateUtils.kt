c479332ac02740deccc24deda19175f023ae1f94
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-11 17:12:01 -0400

    Date and time format

--- app/src/main/java/com/brmsdi/gcsystem/ui/utils/DateUtils.kt
+++ app/src/main/java/com/brmsdi/gcsystem/ui/utils/DateUtils.kt
@@ -1,12 +1,8 @@
 package com.brmsdi.gcsystem.ui.utils
 
-import android.os.Build
 import java.text.SimpleDateFormat
-import java.time.ZoneId
-import java.time.format.DateTimeFormatter
 import java.util.Date
 import java.util.Locale
-
 
 /**
  *
@@ -16,12 +12,7 @@ import java.util.Locale
 class DateUtils private constructor() {
     companion object {
         fun dateFormattedToView(date: Date): String {
-            val format = "dd/MM/yyyy"
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                val formatter = DateTimeFormatter.ofPattern(format)
-                val localDate = Date().toInstant().atZone(ZoneId.systemDefault()).toLocalDate()
-                return formatter.format(localDate)
-            }
+            val format = "dd/MM/yyyy HH:mm"
             val formatter = SimpleDateFormat(format, Locale("pt", "BR"))
             return formatter.format(date)
         }
