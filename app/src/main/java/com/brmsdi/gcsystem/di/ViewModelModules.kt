2aeea73453d3d14a5cf4251eea3952ad9c84c3ae
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-10 21:00:09 -0400

    Update repair request view model di

--- app/src/main/java/com/brmsdi/gcsystem/di/ViewModelModules.kt
+++ app/src/main/java/com/brmsdi/gcsystem/di/ViewModelModules.kt
@@ -2,6 +2,7 @@ package com.brmsdi.gcsystem.di
 
 import com.brmsdi.gcsystem.ui.activity.detailRepairRequest.DetailRepairRequestViewModel
 import com.brmsdi.gcsystem.ui.activity.newRepairRequest.NewRepairRequestViewModel
+import com.brmsdi.gcsystem.ui.activity.updateRepairRequest.UpdateRepairRequestViewModel
 import com.brmsdi.gcsystem.ui.fragment.contract.ContractViewModel
 import com.brmsdi.gcsystem.ui.fragment.debt.DebtViewModel
 import com.brmsdi.gcsystem.ui.fragment.repairRequest.RepairRequestViewModel
@@ -12,6 +13,7 @@ val ViewModelModules = module {
     viewModel { NewRepairRequestViewModel(get(), get()) }
     viewModel { RepairRequestViewModel(get(), get()) }
     viewModel { DetailRepairRequestViewModel(get(), get()) }
+    viewModel { UpdateRepairRequestViewModel(get(), get()) }
     viewModel { DebtViewModel(get(), get()) }
     viewModel { ContractViewModel(get(), get()) }
 }
