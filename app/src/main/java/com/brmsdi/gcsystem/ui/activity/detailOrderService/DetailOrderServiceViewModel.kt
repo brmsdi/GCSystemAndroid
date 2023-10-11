98a454d1951f559a0be279ad375428f34b824654
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-11 19:55:43 -0400

    Created

---
+++ app/src/main/java/com/brmsdi/gcsystem/ui/activity/detailOrderService/DetailOrderServiceViewModel.kt
@@ -0,0 +1,14 @@
+package com.brmsdi.gcsystem.ui.activity.detailOrderService
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.brmsdi.gcsystem.data.service.OrderServiceService
+
+
+/**
+ *
+ * @author Wisley Bruno Marques França
+ * @since 1
+ */
+class DetailOrderServiceViewModel(application: Application, orderServiceService: OrderServiceService) : AndroidViewModel(application) {
+}
