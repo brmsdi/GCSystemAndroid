4fa881c222c6250037b543b9ee99e6a111ebc7ab
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-11 23:08:32 -0400

    Method remove a item in the repair request

--- app/src/main/java/com/brmsdi/gcsystem/ui/activity/detailOrderService/DetailOrderServiceViewModel.kt
+++ app/src/main/java/com/brmsdi/gcsystem/ui/activity/detailOrderService/DetailOrderServiceViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.brmsdi.gcsystem.R
+import com.brmsdi.gcsystem.data.dto.ResponseDTO
 import com.brmsdi.gcsystem.data.dto.ResponseRequestDTO
 import com.brmsdi.gcsystem.data.dto.ValidationModelDTO
 import com.brmsdi.gcsystem.data.listeners.APIEvent
@@ -35,6 +36,8 @@ class DetailOrderServiceViewModel(
     val itemAdded: LiveData<Item> = _itemAdded
     private val _errorItem = MutableLiveData<ValidationModelDTO>()
     val errorItem: LiveData<ValidationModelDTO> = _errorItem
+    private val _removed = MutableLiveData<ResponseDTO>()
+    val removed: LiveData<ResponseDTO> = _removed
     fun details(id: Int) {
         orderServiceRepository.details(id, object : APIEvent<OrderService> {
             override fun onResponse(model: OrderService) {
@@ -90,6 +93,36 @@ class DetailOrderServiceViewModel(
                         ValidationModelDTO(message)
                 }
             }
+        })
+    }
+
+    fun removeAItemInTheRepairRequest(idRepairRequest: Int, idItem: Int) {
+        repairRequestRepository.removeItem(idRepairRequest, idItem, object : APIEvent<ResponseDTO> {
+            override fun onResponse(model: ResponseDTO) {
+                _removed.value = model
+            }
+
+            override fun onError(response: Response<ResponseDTO>) {
+                response.errorBody()?.string()?.let {
+                    val responseRequestDTO =
+                        TextUtils.jsonToObject(it, ResponseRequestDTO::class.java)
+                    _errorItem.value = ValidationModelDTO(responseRequestDTO.errors[0].message)
+                }
+            }
+
+            override fun onFailure(throwable: Throwable) {
+                val cause = throwable.cause
+                if (cause is ConnectException) {
+                    _errorItem.value =
+                        ValidationModelDTO(getApplication<Application>().getString(R.string.ERROR_CONNECTION))
+                } else {
+                    val message = throwable.message
+                        ?: getApplication<Application>().getString(R.string.ERROR_UNEXPECTED)
+                    _errorItem.value =
+                        ValidationModelDTO(message)
+                }
+            }
+
         })
     }
 }
