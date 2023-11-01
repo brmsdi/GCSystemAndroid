911e820d44f54ccbd34a2b23653d07102dfa83a0
Wisley Bruno <srwisley.dev@gmail.com>
2023-10-31 22:39:17 -0400

    Removed field error

--- app/src/main/java/com/brmsdi/gcsystem/ui/fragment/debt/DebtViewModel.kt
+++ app/src/main/java/com/brmsdi/gcsystem/ui/fragment/debt/DebtViewModel.kt
@@ -2,22 +2,16 @@ package com.brmsdi.gcsystem.ui.fragment.debt
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
-import androidx.lifecycle.LiveData
-import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import androidx.paging.cachedIn
 import com.brmsdi.gcsystem.data.constants.Constant.PARAMS.ITEMS_PER_PAGE
-import com.brmsdi.gcsystem.data.dto.ValidationModelDTO
 import com.brmsdi.gcsystem.data.model.Debt
 import com.brmsdi.gcsystem.data.repository.DebtRepository
 import kotlinx.coroutines.flow.Flow
 class DebtViewModel(application: Application, private val debtRepository: DebtRepository) : AndroidViewModel(application) {
-    private val _error = MutableLiveData<ValidationModelDTO>()
-    val error: LiveData<ValidationModelDTO> = _error
-
     val items: Flow<PagingData<Debt>> = Pager(
         config = PagingConfig(pageSize = ITEMS_PER_PAGE, enablePlaceholders = false),
         pagingSourceFactory = { debtRepository.pagingSource() }
